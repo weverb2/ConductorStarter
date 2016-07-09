66f07ddb7f9c9a49f4ffa368b66574841d81b3f3
Brandon Wever <brandon@madebyatomicrobot.com>
2016-07-09 19:23:40 -0400

    Formatting

--- app/src/main/java/com/brandonwever/android/conductorstarter/data/model/Report.kt
+++ app/src/main/java/com/brandonwever/android/conductorstarter/data/model/Report.kt
@@ -3,28 +3,7 @@ package com.brandonwever.android.conductorstarter.data.model
 import auto.parcelgson.AutoParcelGson
 
 @AutoParcelGson abstract class Report {
-
-  /**
-   * "terrestrial_date": "2016-07-01",
-  "sol": 1387,
-  "ls": 177.0,
-  "min_temp": -74.0,
-  "min_temp_fahrenheit": -101.2,
-  "max_temp": 2.0,
-  "max_temp_fahrenheit": 35.6,
-  "pressure": 755.0,
-  "pressure_string": "Lower",
-  "abs_humidity": null,
-  "wind_speed": null,
-  "wind_direction": "--",
-  "atmo_opacity": "Sunny",
-  "season": "Month 6",
-  "sunrise": "2016-07-01T10:20:00Z",
-  "sunset": "2016-07-01T22:20:00Z"
-   */
-
   abstract fun terrestrialDate(): String
-
   abstract fun sol(): Int
   abstract fun ls(): Double
   abstract fun minTemp(): Double
@@ -37,5 +16,4 @@ import auto.parcelgson.AutoParcelGson
   abstract fun season(): String
   abstract fun sunrise(): String
   abstract fun sunset(): String
-
 }
