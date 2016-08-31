639c2ea0e282fda294c0e925e004a2f4de81dea5
Brandon Wever <brandon@madebyatomicrobot.com>
2016-08-30 21:23:37 -0400

    Move view Declaration back to controller

--- app/src/main/java/com/brandonwever/android/conductorstarter/ui/SecondController.kt
+++ app/src/main/java/com/brandonwever/android/conductorstarter/ui/SecondController.kt
@@ -1,16 +1,25 @@
 package com.brandonwever.android.conductorstarter.ui
 
+import android.support.v4.content.ContextCompat
+import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.bluelinelabs.conductor.Controller
+import com.brandonwever.android.conductorstarter.R
 import com.brandonwever.android.conductorstarter.app.App
 import com.brandonwever.android.conductorstarter.data.Action
 import com.brandonwever.android.conductorstarter.data.AppState
 import com.brandonwever.android.conductorstarter.data.RxStore
 import com.brandonwever.android.conductorstarter.data.lcbo.LCBOActionCreator
 import com.brandonwever.android.conductorstarter.data.lcbo.LCBOInteractor
+import org.jetbrains.anko.AnkoComponent
 import org.jetbrains.anko.AnkoContext
+import org.jetbrains.anko.appcompat.v7.navigationIconResource
+import org.jetbrains.anko.appcompat.v7.toolbar
+import org.jetbrains.anko.backgroundColor
+import org.jetbrains.anko.recyclerview.v7.recyclerView
+import org.jetbrains.anko.verticalLayout
 import rx.android.schedulers.AndroidSchedulers
 import timber.log.Timber
 import javax.inject.Inject
@@ -52,4 +61,30 @@ class SecondController : Controller, PaginationDelegate, View.OnClickListener {
     override fun loadMore() {
         actionCreator.getNextPage()
     }
+
+    class SecondControllerView(val paginationDelegate: PaginationDelegate, val listAdapter: ProductListingAdapter, val navigationClickListener: View.OnClickListener) : AnkoComponent<SecondController> {
+        companion object {
+            val RECYCLER_VIEW = 1
+        }
+
+        override fun createView(ui: AnkoContext<SecondController>) = with(ui) {
+            verticalLayout {
+                toolbar() {
+                    setTitleTextColor(ContextCompat.getColor(ctx, android.R.color.white))
+                    title = "Second Controller"
+                    backgroundColor = ContextCompat.getColor(ctx, R.color.colorPrimary)
+                    navigationIconResource = R.drawable.ic_menu
+                    setNavigationOnClickListener(navigationClickListener)
+                }
+                recyclerView {
+                    id = RECYCLER_VIEW
+                    val linearLayoutManager = LinearLayoutManager(ctx, LinearLayoutManager.VERTICAL, false)
+                    adapter = listAdapter
+                    layoutManager = linearLayoutManager
+                    addOnScrollListener(EndlessScrollListener(linearLayoutManager, paginationDelegate))
+                }
+            }
+        }
+    }
+
 }
