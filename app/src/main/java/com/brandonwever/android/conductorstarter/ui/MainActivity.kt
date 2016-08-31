e3b4ae6e40af6b24e2346e5d49f87efa5491479b
Brandon Wever <brandon@madebyatomicrobot.com>
2016-08-30 22:02:57 -0400

    Couldn't get anko working well with status bar drawer overlap, going with xml and Kotlin view binding instead.

--- app/src/main/java/com/brandonwever/android/conductorstarter/ui/MainActivity.kt
+++ app/src/main/java/com/brandonwever/android/conductorstarter/ui/MainActivity.kt
@@ -9,33 +9,24 @@ import android.view.ViewGroup
 import com.bluelinelabs.conductor.*
 import com.brandonwever.android.conductorstarter.R
 import com.brandonwever.android.conductorstarter.app.App
-import com.brandonwever.android.conductorstarter.ui.util.changeHandlerFrameLayout
-import org.jetbrains.anko.AnkoComponent
-import org.jetbrains.anko.AnkoContext
-import org.jetbrains.anko.design.navigationView
-import org.jetbrains.anko.matchParent
-import org.jetbrains.anko.support.v4.drawerLayout
-import org.jetbrains.anko.wrapContent
+import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
+
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener, NavDrawerOwner.View, ControllerChangeHandler.ControllerChangeListener {
 
     lateinit var router: Router
     @Inject lateinit var navDrawerOwner: NavDrawerOwner
 
-    @BindView(R.id.app_container) lateinit var viewGroup: ViewGroup
-    @BindView(R.id.navigation_view) lateinit var navigationView: NavigationView
-    @BindView(R.id.drawer_layout) lateinit var drawerLayout: DrawerLayout
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         App.graph.inject(this)
 
         setContentView(R.layout.activity_main)
-        ButterKnife.bind(this)
+
         navigationView.setNavigationItemSelectedListener(this)
         navDrawerOwner.takeView(this)
-        router = Conductor.attachRouter(this, viewGroup, savedInstanceState)
+        router = Conductor.attachRouter(this, appContainer, savedInstanceState)
         router.addChangeListener(this)
 
         if (!router.hasRootController()) {
@@ -96,36 +87,4 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     override fun onChangeStarted(to: Controller?, from: Controller?, isPush: Boolean, container: ViewGroup?, handler: ControllerChangeHandler?) {
     }
 
-//    <com.bluelinelabs.conductor.ChangeHandlerFrameLayout
-//    android:id="@+id/app_container"
-//    android:layout_height="match_parent"
-//    android:layout_width="match_parent"
-//    tools:context=".ui.MainActivity">
-//    </com.bluelinelabs.conductor.ChangeHandlerFrameLayout>
-//    <android.support.design.widget.NavigationView
-//    android:id="@+id/navigation_view"
-//    android:layout_height="match_parent"
-//    android:layout_width="wrap_content"
-//    app:menu="@menu/navigation_menu"
-//    android:fitsSystemWindows="true"
-//    android:layout_gravity="start"/>
-//    </android.support.v4.widget.DrawerLayout>
-
-
-    class MainActivityView : AnkoComponent<MainActivity> {
-        override fun createView(ui: AnkoContext<MainActivity>) = with(ui) {
-            drawerLayout {
-                layoutParams = ViewGroup.LayoutParams(matchParent, matchParent)
-                fitsSystemWindows = true
-
-                changeHandlerFrameLayout {
-                    layoutParams = ViewGroup.LayoutParams(matchParent, matchParent)
-                }
-                navigationView {
-                    layoutParams = ViewGroup.LayoutParams(wrapContent, matchParent)
-                }
-            }
-        }
-
-    }
 }
