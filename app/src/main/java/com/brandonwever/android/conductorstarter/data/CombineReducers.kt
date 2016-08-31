904da68fe0918b05f36b4b2cd5e92dd9e6a9008b
Brandon Wever <brandon@madebyatomicrobot.com>
2016-08-30 21:11:16 -0400

    Clean up

--- app/src/main/java/com/brandonwever/android/conductorstarter/data/CombineReducers.kt
+++ app/src/main/java/com/brandonwever/android/conductorstarter/data/CombineReducers.kt
@@ -6,8 +6,6 @@ class CombineReducers<S, A> @Inject constructor(vararg val reducers: Reducer<S, 
     override fun call(state: S, action: A): S {
         var newState = state
         for (reducer: Reducer<S, A> in reducers) {
-//            Timber.d(reducer.javaClass.simpleName)
-//            Timber.d(newState.toString())
             newState = reducer.call(newState, action)
         }
         return newState
